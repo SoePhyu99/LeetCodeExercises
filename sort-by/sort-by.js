c4f92714c5079513bc69ab61dadfd2db31214353
Soe Phyu Phyu Htun <133002857+SoePhyu99@users.noreply.github.com>
2023-06-16 15:27:05 +0630

    Time: 158 ms (36.72%) | Memory: 56.3 MB (64.06%) - LeetSync

--- sort-by/sort-by.js
+++ sort-by/sort-by.js
@@ -7,6 +7,7 @@ var sortBy = function(arr, fn) {
     arr.sort(function(a,b){
         let first = fn(a);
         let second = fn(b);
+
         if(first > second) return 1;
         if(first < second) return -1;
         return 0;

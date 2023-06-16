1d118c2dff37ba5553b662a7cda8f53705498a11
Soe Phyu Phyu Htun <133002857+SoePhyu99@users.noreply.github.com>
2023-06-16 10:07:55 +0630

    Time: 72 ms (58.43%) | Memory: 43.4 MB (45.19%) - LeetSync

--- function-composition/function-composition.js
+++ function-composition/function-composition.js
@@ -6,7 +6,6 @@ var compose = function(functions) {
 	return function(x) {
         for(let i = functions.length - 1; i >=0 ; i--){
             x = functions[i](x);
-            console.log(x);
         }
         return x;
     }

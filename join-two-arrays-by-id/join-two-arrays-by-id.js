7e0206213b8ff7eb930bff169ff14ff7912ee12a
Soe Phyu Phyu Htun <133002857+SoePhyu99@users.noreply.github.com>
2023-06-16 14:39:39 +0630

    Time: 7015 ms (5.07%) | Memory: 89.3 MB (98.31%) - LeetSync

---
+++ join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    
+    let arr = [];
+    arr1.forEach(element => {
+        arr.push(element["id"]);
+    });
+    arr2.forEach((element) => {
+        if (arr.includes(element["id"])) {
+            Object.assign(arr1[arr.indexOf(element["id"])], element);
+        } else {
+            arr1.push(element);
+        }
+    })
+
+    return arr1.sort(function (a, b) {
+        let ar = a.id;
+        let br = b.id;
+
+        if (ar > br) return 1;
+        if (ar < br) return -1;
+        return 0;
+    });
+};

f579f4bbb4683d18411a952c4b234d185459f67c
Soe Phyu Phyu Htun <133002857+SoePhyu99@users.noreply.github.com>
2023-07-25 10:26:01 +0630

    Time: 2 ms (6.41%) | Memory: 55.6 MB (41.53%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int result = 0;
+       for(int i = 1; i < arr.length - 1; i++){
+           if(arr[i - 1] < arr[i] && arr[i] > arr[i + 1]){
+               result = i;
+               return i;
+           }
+       }
+       return result;
+    }
+}
